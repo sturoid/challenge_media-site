f16b07c231592b5174b6b6b8131d149026f8ef27
Stuart Clove <stuart.clove@gmail.com>
2018-07-15 11:58:30 -0600

    WIP fix some eslint issues.

---
+++ client/src/components/users/UserWorkExperience/UserWorkExperience.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserWorkExperience = () => (
+  <div>
+    <h1>Work Experience</h1>
+  </div>
+);
+
+export default UserWorkExperience;
---
+++ client/src/components/users/UserWorkExperience/UserWorkExperienceData.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Loader from '../../../components/_shared/Loader';
+import UserWorkExperience from './UserWorkExperience';
+
+class UserWorkExperienceData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      error: false,
+      work: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDataAndSetState();
+  }
+
+  async fetchDataAndSetState() {
+    const { data: work } = await axios.get(
+      `/api/v1/user/${this.props.userId}/work-experience`
+    );
+    this.setState({ work });
+  }
+
+  render() {
+    const { loading, error, work } = this.state;
+    if (loading) return <Loader loading={loading} />;
+    if (error) return <div>{error.message}</div>;
+    return <UserWorkExperience work={work} />;
+  }
+}
+
+UserWorkExperienceData.propTypes = {
+  userId: PropTypes.string.isRequired
+};
+
+export default UserWorkExperienceData;
